0eecf5dd7ca55c1ac70a122b1fde261bdc79adf4
Atanas Bayraktarov <52165081+exaddon@users.noreply.github.com>
2021-11-10 09:36:52 +0100

    .
    .

---
+++ theme_exaddon/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Exaddon Theme',
+    'description': 'Exaddon Theme',
+    'category': 'Theme/Services',
+    'summary': 'Corporate, Business, Tech, Services',
+    'sequence': 120,
+    'version': '1.1',
+    'depends': ['theme_common', 'website_animate'],
+    'data': [
+        'views/assets.xml',
+        'views/cookie_banner.xml',
+        'views/customize_modal.xml',
+        'views/image_content.xml',
+        'views/image_library.xml',
+        'views/snippets_options.xml',
+        'views/snippets.xml',
+    ],
+    'images': [
+        'static/description/Exaddon_description.jpg',
+        'static/description/exaddon_screenshot.jpg',
+    ],
+    'price': 199,
+    'currency': 'EUR',
+    'live_test_url': 'https://theme-clean.odoo.com/page/demo_page_home',
+}
