3f1496d84095c6aa3f35964a3d0100257f6a3a1e
shriya <shriya.pandya@apptware.com>
2021-12-16 16:31:58 +0530

    module for font

---
+++ futura_font 2/__manifest__.py
@@ -0,0 +1,18 @@
+{
+
+    'name': 'Futura font',
+    'category': 'Theme/Ecommers',
+    'summary': 'A module that manages all the process of futura font',
+    'version': '1.0',
+    'author': 'Techultra Solution',
+    'website': 'www.techultrasolutions.com',
+
+    'depends':['base','website'],
+    'data':[
+        'views/assets.xml',
+
+    ],
+
+    'application': True,
+    'auto_install':False,
+}
