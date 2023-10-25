02eae58dca934034bdadc36f3c529e4e90148f04
Exaddon <it_admin@exaddon.com>
2023-10-25 13:24:37 +0200

    empty base

--- bizople_purchase_exaddon/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Developed by Bizople Solutions Pvt. Ltd.
-# See LICENSE file for full copyright and licensing details
-
-from . import purchase
-from . import product_variant
-from . import stock_quant
