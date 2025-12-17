cca713a992a59dc2be121669fba0b56936a30b5d
exaddon <52165081+exaddon@users.noreply.github.com>
2025-12-17 12:11:12 +0100

    .

--- hr_overtime_parttime/models/hr_attendance.py
+++ hr_overtime_parttime/models/hr_attendance.py
@@ -5,15 +5,11 @@
 import pytz
 from collections import defaultdict
 from datetime import datetime, timedelta
-from operator import itemgetter
 from pytz import timezone
 
 from odoo.http import request
 from odoo import models, fields, api, exceptions, _
 from odoo.addons.resource.models.utils import Intervals
-from odoo.osv.expression import AND, OR
-from odoo.tools.float_utils import float_is_zero
-from odoo.tools import format_duration, format_time, format_datetime
 
 
 def get_google_maps_url(latitude, longitude):
@@ -21,20 +17,10 @@ def get_google_maps_url(latitude, longitude):
 
 
 class HrAttendance(models.Model):
-    _name = "hr.attendance"
-    _description = "Attendance"
-    _order = "check_in desc"
-    _inherit = "mail.thread"
+    _inherit = "hr.attendance"  # <--- Extend existing model
+    _description = "Attendance Overtime Extension"
 
-    def _default_employee(self):
-        return self.env.user.employee_id
-
-    employee_id = fields.Many2one('hr.employee', string="Employee", default=_default_employee, required=True, ondelete='cascade', index=True)
-    department_id = fields.Many2one('hr.department', string="Department", related="employee_id.department_id", readonly=True)
-    check_in = fields.Datetime(string="Check In", default=fields.Datetime.now, required=True, tracking=True)
-    check_out = fields.Datetime(string="Check Out", tracking=True)
-    worked_hours = fields.Float(string='Worked Hours', compute='_compute_worked_hours', store=True, readonly=True)
-    color = fields.Integer(compute='_compute_color')
+    # New fields
     overtime_hours = fields.Float(string="Over Time", compute='_compute_overtime_hours', store=True)
     in_latitude = fields.Float(string="Latitude", digits=(10, 7), readonly=True)
     in_longitude = fields.Float(string="Longitude", digits=(10, 7), readonly=True)
@@ -42,29 +28,18 @@ class HrAttendance(models.Model):
     in_city = fields.Char(string="City", readonly=True)
     in_ip_address = fields.Char(string="IP Address", readonly=True)
     in_browser = fields.Char(string="Browser", readonly=True)
-    in_mode = fields.Selection(string="Mode",
-                               selection=[('kiosk', "Kiosk"), ('systray', "Systray"), ('manual', "Manual")],
-                               readonly=True,
-                               default='manual')
+    in_mode = fields.Selection([('kiosk', "Kiosk"), ('systray', "Systray"), ('manual', "Manual")], default='manual', readonly=True)
     out_latitude = fields.Float(digits=(10, 7), readonly=True)
     out_longitude = fields.Float(digits=(10, 7), readonly=True)
     out_country_name = fields.Char(help="Based on IP Address", readonly=True)
     out_city = fields.Char(readonly=True)
     out_ip_address = fields.Char(readonly=True)
     out_browser = fields.Char(readonly=True)
-    out_mode = fields.Selection(selection=[('kiosk', "Kiosk"), ('systray', "Systray"), ('manual', "Manual")],
-                                readonly=True,
-                                default='manual')
-
-    def _compute_color(self):
-        for attendance in self:
-            if attendance.check_out:
-                attendance.color = 1 if attendance.worked_hours > 16 else 0
-            else:
-                attendance.color = 1 if attendance.check_in < (datetime.today() - timedelta(days=1)) else 10
+    out_mode = fields.Selection([('kiosk', "Kiosk"), ('systray', "Systray"), ('manual', "Manual")], default='manual', readonly=True)
 
     @api.depends('worked_hours')
     def _compute_overtime_hours(self):
+        """Compute the overtime hours for each attendance based on hr.attendance.overtime"""
         att_progress_values = dict()
         negative_overtime_attendances = defaultdict(lambda: False)
         if self.employee_id:
@@ -94,13 +69,12 @@ class HrAttendance(models.Model):
                                            AT TIME ZONE etz.timezone
                                   as date)) = date_trunc('day', ot.date)
                    AND att.employee_id = ot.employee_id
-                   AND att.employee_id IN %s
                    AND (ot.adjustment IS false OR ot.adjustment IS NULL)
               ORDER BY att.check_in DESC
-            ''', (tuple(self.employee_id.ids),))
-            a = self.env.cr.dictfetchall()
+            ''')
+            rows = self.env.cr.dictfetchall()
             grouped_dict = dict()
-            for row in a:
+            for row in rows:
                 if row['ot_id'] and row['att_wh']:
                     if row['ot_id'] not in grouped_dict:
                         grouped_dict[row['ot_id']] = {'attendances': [(row['att_id'], row['att_wh'])], 'overtime_duration': row['ot_d']}
@@ -131,108 +105,37 @@ class HrAttendance(models.Model):
             else:
                 attendance.overtime_hours = attendance.worked_hours * ((100 - att_progress_values.get(attendance.id, 100)) / 100)
 
-    @api.depends('employee_id', 'check_in', 'check_out')
-    def _compute_display_name(self):
-        tz = request.httprequest.cookies.get('tz') if request else None
-        for attendance in self:
-            if not attendance.check_out:
-                attendance.display_name = _(
-                    "From %s",
-                    format_time(self.env, attendance.check_in, time_format=None, tz=tz, lang_code=self.env.lang),
-                )
-            else:
-                attendance.display_name = _(
-                    "%s : (%s-%s)",
-                    format_duration(attendance.worked_hours),
-                    format_time(self.env, attendance.check_in, time_format=None, tz=tz, lang_code=self.env.lang),
-                    format_time(attendance.check_out, time_format=None, tz=tz, lang_code=self.env.lang),
-                )
-
-    def _get_employee_calendar(self):
-        self.ensure_one()
-        return self.employee_id.resource_calendar_id or self.employee_id.company_id.resource_calendar_id
-
-    @api.depends('check_in', 'check_out')
-    def _compute_worked_hours(self):
-        for attendance in self:
-            if attendance.check_out and attendance.check_in and attendance.employee_id:
-                calendar = attendance._get_employee_calendar()
-                resource = attendance.employee_id.resource_id
-                tz = timezone(calendar.tz)
-                check_in_tz = attendance.check_in.astimezone(tz)
-                check_out_tz = attendance.check_out.astimezone(tz)
-                lunch_intervals = attendance.employee_id._employee_attendance_intervals(check_in_tz, check_out_tz, lunch=True)
-                attendance_intervals = Intervals([(check_in_tz, check_out_tz, attendance)]) - lunch_intervals
-                delta = sum((i[1] - i[0]).total_seconds() for i in attendance_intervals)
-                attendance.worked_hours = delta / 3600.0
-            else:
-                attendance.worked_hours = False
-
-    @api.constrains('check_in', 'check_out')
-    def _check_validity_check_in_check_out(self):
-        for attendance in self:
-            if attendance.check_in and attendance.check_out:
-                if attendance.check_out < attendance.check_in:
-                    raise exceptions.ValidationError(_('"Check Out" time cannot be earlier than "Check In" time.'))
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        res = super().create(vals_list)
-        res._update_overtime()
-        return res
-
-    def write(self, vals):
-        attendances_dates = self._get_attendances_dates()
-        result = super(HrAttendance, self).write(vals)
-        if any(field in vals for field in ['employee_id', 'check_in', 'check_out']):
-            for emp, dates in self._get_attendances_dates().items():
-                attendances_dates[emp] |= dates
-            self._update_overtime(attendances_dates)
-        return result
-
-    def unlink(self):
-        attendances_dates = self._get_attendances_dates()
-        res = super().unlink()
-        self._update_overtime(attendances_dates)
-        return res
-
-    def _get_day_start_and_day(self, employee, dt):
-        if not dt.tzinfo:
-            date_employee_tz = pytz.utc.localize(dt).astimezone(pytz.timezone(employee._get_tz()))
-        else:
-            date_employee_tz = dt
-        start_day_employee_tz = date_employee_tz.replace(hour=0, minute=0, second=0)
-        return (start_day_employee_tz.astimezone(pytz.utc).replace(tzinfo=None), start_day_employee_tz.date())
-
     def _get_attendances_dates(self):
+        """Helper to gather employee attendance dates for overtime recalculation"""
+        from datetime import datetime
+        from collections import defaultdict
         attendances_emp = defaultdict(set)
         for attendance in self.filtered(lambda a: a.employee_id.company_id.hr_attendance_overtime and a.check_in):
-            check_in_day_start = attendance._get_day_start_and_day(attendance.employee_id, attendance.check_in)
-            if check_in_day_start[0] < datetime.combine(attendance.employee_id.company_id.overtime_start_date, datetime.min.time()):
-                continue
-            attendances_emp[attendance.employee_id].add(check_in_day_start)
+            check_in_day_start = attendance.check_in.replace(hour=0, minute=0, second=0, microsecond=0)
+            attendances_emp[attendance.employee_id].add((check_in_day_start, check_in_day_start.date()))
             if attendance.check_out:
-                check_out_day_start = attendance._get_day_start_and_day(attendance.employee_id, attendance.check_out)
-                attendances_emp[attendance.employee_id].add(check_out_day_start)
+                check_out_day_start = attendance.check_out.replace(hour=0, minute=0, second=0, microsecond=0)
+                attendances_emp[attendance.employee_id].add((check_out_day_start, check_out_day_start.date()))
         return attendances_emp
 
     def _update_overtime(self, employee_attendance_dates=None):
+        """Recalculate overtime for given employee attendance dates"""
         if employee_attendance_dates is None:
             employee_attendance_dates = self._get_attendances_dates()
 
         overtime_to_unlink = self.env['hr.attendance.overtime']
         overtime_vals_list = []
-        affected_employees = self.env['hr.employee']
+
         for emp, attendance_dates in employee_attendance_dates.items():
             for day_data in attendance_dates:
                 day = day_data[0]
                 attendances = self.env['hr.attendance'].search([
                     ('employee_id', '=', emp.id),
                     ('check_in', '>=', day),
-                    ('check_in', '<', day + timedelta(days=1))  # <-- Fixed here
+                    ('check_in', '<', day + timedelta(days=1))
                 ])
                 total_hours = sum(att.worked_hours or 0 for att in attendances)
-                # Create or update overtime record
+
                 overtime = self.env['hr.attendance.overtime'].sudo().search([
                     ('employee_id', '=', emp.id),
                     ('date', '=', day_data[1]),
@@ -250,23 +153,5 @@ class HrAttendance(models.Model):
                 elif overtime:
                     overtime_to_unlink |= overtime
 
-        created_overtimes = self.env['hr.attendance.overtime'].sudo().create(overtime_vals_list)
+        self.env['hr.attendance.overtime'].sudo().create(overtime_vals_list)
         overtime_to_unlink.sudo().unlink()
-        self.env.add_to_compute(self._fields['overtime_hours'],
-                                self.search([('employee_id', 'in', [e.id for e in employee_attendance_dates.keys()])]))
-
-    def action_in_attendance_maps(self):
-        self.ensure_one()
-        return {
-            'type': 'ir.actions.act_url',
-            'url': get_google_maps_url(self.in_latitude, self.in_longitude),
-            'target': 'new'
-        }
-
-    def action_out_attendance_maps(self):
-        self.ensure_one()
-        return {
-            'type': 'ir.actions.act_url',
-            'url': get_google_maps_url(self.out_latitude, self.out_longitude),
-            'target': 'new'
-        }
