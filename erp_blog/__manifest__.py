fd0aebcb1b9bc263f06b74fe74cb4944e35345cb
Aleksandr Zurnachyan <aleksandrzurnachyan@MacBook-Air-Aleksandr.local>
2026-03-04 17:44:21 +0400

    aadd

---
+++ erp_blog/__manifest__.py
@@ -0,0 +1,20 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'ERP Blog',
+    'author': 'ERP SWISS',
+    'version': '1.0',
+    'description': """Erpswiss""",
+    'depends': ['website_blog'],
+    'data': [
+        'views/blog_styles.xml',
+        'views/blog_post_view.xml'
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            'erp_blog/static/src/scss/blog.scss',
+        ]
+    },
+    'application': True,
+    'installable': True,
+}
