8c68e10b1a62fab97d8b6830166089fa8ee5f50f
Aleksandr Zurnachyan <aleksandrzurnachyan@MacBook-Air-Aleksandr.local>
2026-03-16 15:51:49 +0400

    added jobs page

--- erp_blog/__manifest__.py
+++ erp_blog/__manifest__.py
@@ -1,20 +1,21 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'ERP Blog',
+    'name': 'ERP Blog and Recruitment',
     'author': 'ERP SWISS',
     'version': '1.0',
-    'description': """Erpswiss""",
-    'depends': ['website_blog'],
+    'description': """ERP SWISS Recruitment and Blog""",
+    'depends': ['website_blog', 'website_hr_recruitment'],
     'data': [
         'views/blog_styles.xml',
-        'views/blog_post_view.xml'
+        'views/blog_post_view.xml',
+        'views/recruitment_job.xml'
     ],
     'assets': {
         'web.assets_frontend': [
             'erp_blog/static/src/js/blog.js',
             'erp_blog/static/src/scss/blog.scss',
-
+            'erp_blog/static/src/scss/job.scss',
         ]
     },
     'application': True,
